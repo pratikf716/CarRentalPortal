366dca5042a79f99469bbd78e48beb5c98554a9d
pratik7020 <pratikfutane19@gmail.com>
2024-08-03 09:36:25 +0530

    first commit

---
+++ ev/ev.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.book-now').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const carName = this.getAttribute('data-car');
+        const formPageUrl = 'booking.html?car=' + encodeURIComponent(carName);
+        window.location.href = formPageUrl;
+    });
+});
+
+
